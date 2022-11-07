fa9ba6a9be4def8a5d217102fbdb537d49bc3d6d
pspiagicw <pspiagicw@gmail.com>
2022-11-07 13:51:15 +0530

    Added DI based interfacing

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Built for usage in simple personal projects
 - To be used for critical applications where logging is a debugging/auditing requirement.
 
 ## Usage
-This library simply has 8 functions.
+This library simply has 7 simple functions.
 Which follow the familiar `Printf` format.
 
 - LogSuccess: Prints green good stuff
@@ -31,6 +31,24 @@ colorlog.LogFatal("Everything is %s", "fucked")
 colorlog.LogSuccessFancy("Everything is %s", "right")
 colorlog.LogErrorFancy("Everything is %s", "bad")
 colorlog.LogInfoFancy("Everything is %s", "okay")
+```
+
+If needing a dependency injection method. We also provide a `ColorLogger` interface which requires the same 7 functions.
+We provide a `DefaultColorLogger` type for actuall logging. The `ColorLogger` interface can be mocked for testing purposes.
+
+```go
+logger := &colorlog.DefaultColorLogger{}
+
+logger.LogSuccess("Everything is %s", "right")
+logger.LogError("Everything is %s", "bad")
+logger.LogInfo("Everything is %s", "okay")
+logger.LogFatal("Everything is %s", "fucked")
+
+logger.LogSuccessFancy("Everything is %s", "right")
+logger.LogErrorFancy("Everything is %s", "bad")
+logger.LogInfoFancy("Everything is %s", "okay")
+
+
 ```
 
 
--- colorlog.go
+++ colorlog.go
@@ -7,37 +7,98 @@ import (
 	"github.com/fatih/color"
 )
 
-
-
+// Log when something goes right
+// Prints using green color
 func LogSuccess(format string, v ...interface{}) {
-    message := fmt.Sprintf(format, v...)
-    color.Green(message)
+	message := fmt.Sprintf(format, v...)
+	color.Green(message)
 }
 
+// Log when something goes wrong
+// Prints using a red color
 func LogError(format string, v ...interface{}) {
-    message := fmt.Sprintf(format , v...)
+	message := fmt.Sprintf(format, v...)
 
-    color.Red(message)
+	color.Red(message)
 }
+
+// Log when something does very wrong
+// Prints using red color
+// Additionally exits from the application using `os.Exit`
 func LogFatal(format string, v ...interface{}) {
-    message := fmt.Sprintf(format , v...)
-    color.Red(message)
+	message := fmt.Sprintf(format, v...)
+	color.Red(message)
 
-    os.Exit(1)
+	os.Exit(1)
 }
+
+// Log when communicating some info
+// Prints using a yellow color
 func LogInfo(format string, v ...interface{}) {
-    message := fmt.Sprintf(format , v...)
-    color.Yellow(message)
+	message := fmt.Sprintf(format, v...)
+	color.Yellow(message)
 }
+
+// Log something positive
+// Prints in green with a ASCII tick (`✓`)
 func LogSuccessFancy(format string, v ...interface{}) {
 	message := "[*] ✓ " + fmt.Sprintf(format, v...)
 	color.Green(message)
 }
+
+// Log something negative
+// Prints in red with a ASCII X (`x`)
 func LogErrorFancy(format string, v ...interface{}) {
 	message := "[*] x " + fmt.Sprintf(format, v...)
 	color.Red(message)
 }
+
+// Log something informative
+// Prints in yello in ASCII plug (`+`)
 func LogInfoFancy(format string, v ...interface{}) {
 	message := "[*] + " + fmt.Sprintf(format, v...)
 	color.Yellow(message)
+}
+
+// ColorLogger interface for dependency injection purposes.
+// Useful for mocking with custom type.
+type ColorLogger interface {
+    LogSuccess(string, ...interface{})
+    LogError(string, ...interface{})
+    LogInfo(string, ...interface{})
+    LogFatal(string, ...interface{})
+
+    LogSuccessFancy(string, ...interface{})
+    LogErrorFancy(string, ...interface{})
+    LogInfoFancy(string, ...interface{})
+}
+
+
+// Empty struct for implementing the ColorLogger interface.
+type DefaultColorLogger struct {
+}
+
+func (d *DefaultColorLogger) LogSuccess(format string, v ...interface{}) {
+    LogSuccess(format , v...)
+}
+func (d *DefaultColorLogger) LogError(format string, v ...interface{}) {
+    LogError(format , v...)
+}
+func (d *DefaultColorLogger) LogInfo(format string, v ...interface{}) {
+    LogInfo(format, v...)
+}
+func (d *DefaultColorLogger) LogFatal(format string, v ...interface{}) {
+    LogFatal(format, v...)
+}
+
+
+
+func (d *DefaultColorLogger) LogSuccessFancy(format string, v ...interface{}) {
+    LogSuccessFancy(format, v...)
+}
+func (d *DefaultColorLogger) LogErrorFancy(format string, v ...interface{}) {
+    LogErrorFancy(format, v...)
+}
+func (d *DefaultColorLogger) LogInfoFancy(format string, v ...interface{}) {
+    LogInfoFancy(format, v...)
 }
